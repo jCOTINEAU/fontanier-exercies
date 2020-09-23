0bacbc242e182c38bf9beded1d097bf303d6f5db
Jeremy Cotineau <jeremy.cotineau_ext@michelin.com>
2020-09-23 21:04:55 +0200

    kanji mode

--- src/models/WordBlock.js
+++ src/models/WordBlock.js
@@ -1,16 +1,43 @@
 import React, { useState } from 'react';
+import { useContext } from 'react';
+import {displayKanjiContext} from '../contexts/KanjiContext'
+import {displayColor, displayColorContext} from '../contexts/ColorContext'
 
 export default function WordBlock(props)
 {
-  const [s,setS] =  useState(true)
+  const {color, setColor} = useContext(displayColorContext);
+  const {kanji,toggleKanji} = useContext(displayKanjiContext)
+  const [displayTrad,setDisplayTrad] =  useState(false)
+  
+  let toDisplay=props.wb.value;
+
+  if(displayTrad)
+  {
+    if(props.wb.trad)
+    {
+      if(kanji && props.wb.trad.kanji)
+      {
+        toDisplay = props.wb.trad.kanji
+      }else if(props.wb.trad){
+        toDisplay = props.wb.trad.kana
+      }
+    }else{
+      toDisplay=props.wb.value
+    }
+  }else{
+    toDisplay=props.wb.value
+  }
+  
+
   return(
-    <button className='wordBlock' onMouseLeave={()=>{
-      setS(!s)
+    <button className='wordBlock' style={{backgroundColor:color?props.wb.color:"white"}}
+    onMouseLeave={()=>{
+      setDisplayTrad(!displayTrad)
     }} onMouseEnter={()=>{
-      setS(!s)
+      setDisplayTrad(!displayTrad)
 
     }}>
-        {s? props.wb.value:props.wb.trad??props.wb.value}
+        {toDisplay}
     </button>
 
   )
