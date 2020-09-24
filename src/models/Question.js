3afc42353c0a53b715f589e15eb569c4d62e2267
Jeremy Cotineau <jeremy.cotineau_ext@michelin.com>
2020-09-24 21:41:55 +0200

    fix warning unused

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import WordBlock from './WordBlock.js';
-import { makeStyles } from '@material-ui/core/styles';
 
 export default class Question extends React.Component {
 
